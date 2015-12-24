206af4918fc63c69db93a0f8c2350bb4d409e837
Jack Christensen <jack@jackchristensen.com>
2015-12-24 13:20:19 -0600

    Make EncodeText and EncodeBytea type strict

--- conn.go
+++ conn.go
@@ -861,9 +861,9 @@ func (c *Conn) sendPreparedQuery(ps *PreparedStatement, arguments ...interface{}
 		case Encoder:
 			err = arg.Encode(wbuf, oid)
 		case string:
-			err = wbuf.EncodeText(arguments[i])
+			err = wbuf.EncodeText(arg)
 		case []byte:
-			err = wbuf.EncodeBytea(arguments[i])
+			err = wbuf.EncodeBytea(arg)
 		default:
 			if v := reflect.ValueOf(arguments[i]); v.Kind() == reflect.Ptr {
 				if v.IsNil() {
@@ -877,8 +877,6 @@ func (c *Conn) sendPreparedQuery(ps *PreparedStatement, arguments ...interface{}
 			switch oid {
 			case BoolOid:
 				err = wbuf.EncodeBool(arguments[i])
-			case ByteaOid:
-				err = wbuf.EncodeBytea(arguments[i])
 			case Int2Oid:
 				err = wbuf.EncodeInt2(arguments[i])
 			case Int4Oid:
@@ -889,8 +887,6 @@ func (c *Conn) sendPreparedQuery(ps *PreparedStatement, arguments ...interface{}
 				err = wbuf.EncodeFloat4(arguments[i])
 			case Float8Oid:
 				err = wbuf.EncodeFloat8(arguments[i])
-			case TextOid, VarcharOid:
-				err = wbuf.EncodeText(arguments[i])
 			case DateOid:
 				err = wbuf.EncodeDate(arguments[i])
 			case TimestampTzOid:
